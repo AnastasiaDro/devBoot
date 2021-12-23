0c4b70040cf48e6d4c8d319ab7e05781fd10b576
cerebus <adrogunova@gmail.com>
2021-12-23 20:31:30 +0300

    added some topics to main-file

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,28 @@
 /*topics:
     - trimMargin and TrimIndent
     - в тройных кавычках не требюуется экранирование значений
-    -
+    -NULL safety:
+           -элвис оператор
+           -!! - кидает NPE, если нулл
+           -nullable типы = ?
+
+  Разобраться:
+           fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) {
+    val email = client?.personalInfo?.email
+    if (email != null && message != null)
+        mailer.sendMessage(email, message)
+
+
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
  */
 fun main() {
     println("Hello!\nLet's start!")
